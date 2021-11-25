edeb64e10bec2ac5e016bcf63cbab93a5ada388e
Vaseem Raja Kottarath <vaseemrajakottarath@gmail.com>
2021-11-25 22:14:52 +0530

    product order and  payments

--- orders/urls.py
+++ orders/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.orders,name='orders'),
     path('payments',views.payments,name='payments'),
+    path('order_complete',views.order_complete,name='order_complete'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,7 @@ import datetime,json
 from store.models import Product
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -130,9 +131,36 @@ def payments(request):
     send_email = EmailMessage(mail_subject, message, to=[to_email])
     send_email.send()
 
+    # Send order number and transaction id back to sendData method via JsonResponse
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 
+def order_complete(request):
+   order_number = request.GET.get('order_number')
+   transID = request.GET.get('payment_id')
 
+   try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
 
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
 
-    return render(request,'store/payments.html')
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'order_complete.html', context)
+   except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
